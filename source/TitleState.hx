86d521c2b10f696ebd61bf6200d91d17b55f9b30
Itz_MIles <95124554+Itz-Miles@users.noreply.github.com>
2023-08-07 01:16:34 -0600

    placeholder titlestate

---
+++ source/TitleState.hx
@@ -0,0 +1,132 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.input.keyboard.FlxKey;
+import flixel.group.FlxGroup;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+import openfl.Assets;
+
+using StringTools;
+
+class TitleState extends MusicBeatState
+{
+	var logoBl:FlxSprite;
+	var splashText:FlxSprite;
+	var bg:FlxSprite;
+	var titleGF:Character;
+	var transitioning:Bool;
+	var gamepad:FlxGamepad;
+	var pressedEnter:Bool;
+
+	override public function create():Void
+	{
+		super.create();
+
+		bg = new FlxSprite().makeGraphic(128, 72, 0x9ac9ff);
+		bg.origin.set(0, 0);
+		bg.scale.x = bg.scale.y = 10.0;
+		add(bg);
+		/*
+			wondering how simple graphics are handled 
+			but im too busy to figure stuff out for now
+			is it faster to scale a small graphic, or draw a large one?
+		 */
+
+		logoBl = new FlxSprite(-15, -10).loadGraphic(Paths.image('logos/logo', "shared"));
+		logoBl.updateHitbox();
+		logoBl.antialiasing = ClientPrefs.globalAntialiasing;
+		add(logoBl);
+
+		splashText = new FlxSprite(0, 560).loadGraphic(Paths.image('text/splashText', "shared"));
+		splashText.updateHitbox();
+		splashText.antialiasing = ClientPrefs.globalAntialiasing;
+		splashText.screenCenter(X);
+		add(splashText);
+		FlxTween.tween(splashText.scale, {x: 0.76, y: 0.76}, 0.1, {ease: FlxEase.cubeOut, type: FlxTweenType.PERSIST});
+
+		titleGF = new Character(758, 219, "titleGF", false, "shared");
+		add(titleGF);
+
+		FlxG.camera.flash(FlxColor.WHITE, 1.7);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music != null)
+			Conductor.songPosition = FlxG.sound.music.time;
+
+		if (FlxG.keys.justPressed.F)
+		{
+			FlxG.fullscreen = !FlxG.fullscreen;
+		}
+
+		if (FlxG.keys.justPressed.ENTER || controls.ACCEPT)
+			pressedEnter = true;
+
+		#if mobile
+		for (touch in FlxG.touches.list)
+		{
+			if (touch.justPressed)
+			{
+				pressedEnter = true;
+			}
+		}
+		#end
+
+		if (gamepad != null)
+		{
+			if (gamepad.justPressed.START)
+				pressedEnter = true;
+
+			#if switch
+			if (gamepad.justPressed.B)
+				pressedEnter = true;
+			#end
+		}
+
+		if (!transitioning)
+		{
+			if (pressedEnter)
+			{
+				FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+				splashText.y = 570;
+				transitioning = true;
+
+				new FlxTimer().start(1, function(tmr:FlxTimer)
+				{
+					MusicBeatState.switchState(new MainMenuState());
+				});
+			}
+		}
+		super.update(elapsed);
+	}
+
+	function tweenBack(tween:FlxTween):Void
+	{
+		FlxTween.tween(splashText.scale, {x: 0.76, y: 0.76}, 0.165, {ease: FlxEase.cubeOut, type: FlxTweenType.PERSIST});
+		FlxTween.tween(logoBl.scale, {x: 0.975, y: 0.975}, 0.165, {ease: FlxEase.cubeOut, type: FlxTweenType.PERSIST});
+	}
+
+	override function beatHit()
+	{
+		super.beatHit();
+		if (splashText != null)
+			FlxTween.tween(splashText.scale, {x: 0.79, y: 0.79}, 0.165, {type: FlxTweenType.PERSIST, ease: FlxEase.cubeOut, onComplete: tweenBack});
+		if (splashText != null)
+			splashText.y = 560; // might tween back later
+		if (logoBl != null)
+			FlxTween.tween(logoBl.scale, {x: 1, y: 1}, 0.165, {type: FlxTweenType.PERSIST, ease: FlxEase.cubeOut, onComplete: tweenBack});
+
+		if (titleGF != null)
+		{
+			titleGF.dance();
+		}
+	}
+}

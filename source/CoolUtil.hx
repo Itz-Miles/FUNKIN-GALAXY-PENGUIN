027a7ace98d5043d0ecc40e15457bc283bc6f792
Itz_MIles <95124554+Itz-Miles@users.noreply.github.com>
2023-08-07 03:40:49 -0600

    coolutil functions

---
+++ source/CoolUtil.hx
@@ -0,0 +1,191 @@
+package;
+
+import flixel.util.FlxColor;
+import openfl.geom.Point;
+import openfl.geom.Rectangle;
+import openfl.display.BitmapData;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import openfl.utils.Assets;
+import flixel.util.FlxSave;
+
+using StringTools;
+
+class CoolUtil
+{
+	//public static var difficulties:Array<String> = ['Easy', 'Normal', 'Hard'];
+
+	inline public static function quantize(f:Float, snap:Float)
+	{
+		// changed so this actually works lol
+		var m:Float = Math.fround(f * snap);
+		trace(snap);
+		return (m / snap);
+	}
+/*
+	public static function getDifficultyFilePath(difficultyIndex:Null<Int> = null)
+	{
+		if (difficultyIndex == null)
+			difficultyIndex = PlayState.storyDifficulty;
+
+		var fileSuffix:String = difficulties[difficultyIndex];
+		if (fileSuffix != ClientPrefs.optionsDifficulty)
+		{
+			fileSuffix = '-' + fileSuffix;
+		}
+		else
+		{
+			fileSuffix = '';
+		}
+		return Paths.formatToSongPath(fileSuffix);
+	}
+
+	public static function getDifficultyIndex(arr:Array<String>, search:String):Int
+	{
+		var index:Int = 0;
+		for (i in 0...arr.length - 1)
+		{
+			if (arr[i] == search)
+			{
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+
+	public static function getDifficultyString(?difficultyInt):String
+	{
+		if (difficultyInt != null)
+			return difficulties[difficultyInt].toUpperCase();
+
+		return difficulties[PlayState.storyDifficulty].toUpperCase();
+	}
+    */
+
+	inline public static function boundTo(value:Float, min:Float, max:Float):Float
+	{
+		return Math.max(min, Math.min(max, value));
+	}
+/*
+	public static function coolTextFile(path:String):Array<String>
+	{
+		var daList:Array<String> = [];
+		if (Assets.exists(path))
+			daList = Assets.getText(path).trim().split('\n');
+
+		for (i in 0...daList.length)
+		{
+			daList[i] = daList[i].trim();
+		}
+
+		return daList;
+	}
+*/
+
+	public static function listFromString(string:String):Array<String>
+	{
+		var daList:Array<String> = [];
+		daList = string.trim().split('\n');
+
+		for (i in 0...daList.length)
+		{
+			daList[i] = daList[i].trim();
+		}
+
+		return daList;
+	}
+
+	public static function dominantColor(sprite:flixel.FlxSprite):Int
+	{
+		var countByColor:Map<Int, Int> = [];
+		for (col in 0...sprite.frameWidth)
+		{
+			for (row in 0...sprite.frameHeight)
+			{
+				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+				if (colorOfThisPixel != 0)
+				{
+					if (countByColor.exists(colorOfThisPixel))
+					{
+						countByColor[colorOfThisPixel] = countByColor[colorOfThisPixel] + 1;
+					}
+					else if (countByColor[colorOfThisPixel] != 13520687 - (2 * 13520687))
+					{
+						countByColor[colorOfThisPixel] = 1;
+					}
+				}
+			}
+		}
+		var maxCount = 0;
+		var maxKey:Int = 0; // after the loop this will store the max color
+		countByColor[flixel.util.FlxColor.BLACK] = 0;
+		for (key in countByColor.keys())
+		{
+			if (countByColor[key] >= maxCount)
+			{
+				maxCount = countByColor[key];
+				maxKey = key;
+			}
+		}
+		return maxKey;
+	}
+
+	public static function numberArray(max:Int, ?min = 0):Array<Int>
+	{
+		var dumbArray:Array<Int> = [];
+		for (i in min...max)
+		{
+			dumbArray.push(i);
+		}
+		return dumbArray;
+	}
+/*
+	public static function precacheSound(sound:String, ?library:String = null):Void
+	{
+		Paths.sound(sound, library);
+	}
+
+	public static function precacheMusic(sound:String, ?library:String = null):Void
+	{
+		Paths.music(sound, library);
+	}
+*/
+	public static function browserLoad(site:String)
+	{
+		#if linux
+		Sys.command('/usr/bin/xdg-open', [site]);
+		#else
+		FlxG.openURL(site);
+		#end
+	}
+
+	public static function userName():String
+	{
+		var userName:String = "joe";
+		/*
+			#if (sys != null) // Check if running on desktop
+			{
+				userName = sys.User.getCurrentUserName();
+			}
+			#elseif (js != null) // Check if running in JavaScript
+			{
+				userName = js.Browser.window.navigator.userAgent;
+			}
+			#elseif (neko != null) // Check if running on Neko
+			{
+				userName = neko.Sys.getEnv("USER");
+			}
+			#end
+			trace("Current user's name: " + userName);
+		 */
+		return userName;
+	}
+
+	public static function getSavePath(folder:String = 'Itz_Miles'):String
+	{
+		@:privateAccess
+		return '${FlxG.stage.application.meta.get('company')}/${FlxSave.validate(FlxG.stage.application.meta.get('file'))}';
+	}
+}

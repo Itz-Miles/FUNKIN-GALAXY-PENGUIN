c6c744c73c53cac3c825f9949bb59bccb55d4228
Itz_MIles <95124554+Itz-Miles@users.noreply.github.com>
2023-08-08 13:32:44 -0600

    no fancy bracket stuff

--- source/EXT.hx
+++ source/EXT.hx
@@ -8,21 +8,21 @@ class EXT
 
 	public static inline function sound(filename:String):String
 	{
-		return 'assets/sounds/${filename}.${AUDIO_EXT}';
+		return 'assets/sounds/$filename.$AUDIO_EXT';
 	}
 
 	public static inline function music(filename:String):String
 	{
-		return 'assets/music/${filename}.${AUDIO_EXT}';
+		return 'assets/music/$filename.$AUDIO_EXT';
 	}
 
 	public static inline function json(filename:String):String
 	{
-		return 'assets/data/${filename}.${DATA_EXT}';
+		return 'assets/data/$filename.$DATA_EXT';
 	}
 
 	public static inline function xml(filename:String):String
 	{
-		return 'assets/images/${filename}-anim.${ANIM_EXT}';
+		return 'assets/images/$filename-anim.$ANIM_EXT';
 	}
 }
